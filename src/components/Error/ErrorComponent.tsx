fcf80d4fb75c56af7cd6b99ca3e8e9fb33fc2d9e
Andrew Grabynsky <hrabynsky@gmail.com>
2024-08-17 17:15:38 +0300

    17082024

---
+++ src/components/Error/ErrorComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorComponent = () => {
+    return (
+        <div>
+            <h1>ERROR</h1>
+        </div>
+    );
+};
+
+export default ErrorComponent;
--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -3,6 +3,7 @@ import React, {FC, useState} from 'react';
 import "../../app/globals.css";
 import {getSearchMovies} from '@/services/api.services';
 import Link from 'next/link';
+import styles from "./HomeComponent.module.css";
 
 interface ISearchMovie {
     id: number,
@@ -24,30 +25,38 @@ const HomeComponent = () => {
     return (
         <div className='home'>
             <h1>You are welcome on the site MOVIES</h1>
-            <h2>Choose the movie</h2>
+            <h2 className={styles.h2Home}>Choose the movie</h2>
 
+            <label className={styles.labelHome}> Search movie:
+                <input
+                    type="text"
+                    name="searchInput"
+                    className={styles.searchInput}
+                    value={query}
+                    onChange={
+                    (event) =>
+                        setQuery(event.target.value)}
+                />
 
-            <input
-                type="text"
-                name="searchInput"
-                value={query}
-                onChange={(event) => {
-                    setQuery(event.target.value);
-                    event.target.value = ''
-                }}
-            />
+                <button
+                    onClick={searchMovies}
+                    className={styles.btn}
+                >
+                    Search
+                </button>
+            </label>
 
-            <button onClick={searchMovies}>Search</button>
-
-            {
-                movies?.map(movie =>
-                    <Link key={movie.id}
-                          href={{pathname: '/movie' + '/' + movie.id, query:{data: JSON.stringify(movie.id)}}}
-                    >
-                        {movie.name}
-                    </Link>)
-            }
-
+            <div className={styles.searchMovieDiv}>
+                {
+                    movies?.map(movie =>
+                        <Link key={movie.id}
+                              href={{pathname: '/movie' + '/' + movie.id, query: {data: JSON.stringify(movie.id)}}}
+                        className={styles.movieLink}
+                        >
+                            {movie.name}
+                        </Link>)
+                }
+            </div>
         </div>
     );
 };
